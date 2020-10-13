ba2499b1c57ec7a305b59016b2adb0b2aa96d861
otallyto <rodrigues.tallyto@hotmail.com>
2020-10-13 00:50:34 -0400

    Updates

---
+++ precache-manifest.a7b8ce1d6b58bad77850dd5f557fd5d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd09219dc9cf27ad74c02b0522e96a7",
+    "url": "/happy/index.html"
+  },
+  {
+    "revision": "2bf91c80a9b5c2ec5ef5",
+    "url": "/happy/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "cfb825a300f66ae5b079",
+    "url": "/happy/static/css/main.98710c45.chunk.css"
+  },
+  {
+    "revision": "2bf91c80a9b5c2ec5ef5",
+    "url": "/happy/static/js/2.216a380a.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/happy/static/js/2.216a380a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb825a300f66ae5b079",
+    "url": "/happy/static/js/main.75516317.chunk.js"
+  },
+  {
+    "revision": "beffc59a84efbb1596c3",
+    "url": "/happy/static/js/runtime-main.61df2244.js"
+  },
+  {
+    "revision": "6079787b048ebd91365e13e4b2ce6c88",
+    "url": "/happy/static/media/landing.6079787b.svg"
+  },
+  {
+    "revision": "2b00e2c37f462bb51165784525031a20",
+    "url": "/happy/static/media/logo.2b00e2c3.svg"
+  },
+  {
+    "revision": "2cf1ba9f2d1800bce5121dad73579432",
+    "url": "/happy/static/media/map-marker.2cf1ba9f.svg"
+  }
+]);
