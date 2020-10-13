082ed60dd7833097a8557e9e7fb0e8bfb57325d0
otallyto <rodrigues.tallyto@hotmail.com>
2020-10-12 21:22:41 -0400

    feat: create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './assets/logo.svg'
+import {FiArrowRight} from 'react-icons/fi'
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World NLW</h1>
+    <div id="page-landing">
+     <div className="content-wrapper">
+     <img src={logoImg} alt="Happy logo"/>
+     <main>
+       <h1>Leve felicidade para o mundo</h1>
+       <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+     </main>
+     <div className="location">
+       <strong>Boa Vista</strong>
+       <span>Roraima</span>
+     </div>
+     <a href="" className="enter-app">
+       <FiArrowRight size={24} color="rgba(0,0,0,0.6)"/>
+     </a>
+     </div>
     </div>
   );
 }
